4c2d749f9b53b087ab27fc36da3f355dfdf4b13f
LakshRaghav <103411991+LakshRaghav@users.noreply.github.com>
2022-08-25 12:03:58 -0700

    fifth commit

--- config/db.js
+++ config/db.js
@@ -1,6 +1,14 @@
 require('dotenv').config();
-const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGO_URI,{
-    useNewUrlParser:true,
-    useUnifiedTopology:true,
+// const mongoose = require('mongoose');
+// mongoose.connect(process.env.MONGO_URI,{
+//     useNewUrlParser:true,
+//     useUnifiedTopology:true,
+// });
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const client = new MongoClient(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
 });
